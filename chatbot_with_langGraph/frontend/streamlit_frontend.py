8df5357895dba163097973557dcc27f126030622
usman butt <usmanbutt2357@gmail.com>
2026-09-21 15:41:39 +0500

    feat: replace memorysaver with sqlitesaver

--- chatbot_with_langGraph/frontend/streamlit_frontend.py
+++ chatbot_with_langGraph/frontend/streamlit_frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from chatbot_with_langGraph.backend.workflow import workflow
+from chatbot_with_langGraph.backend.workflow import workflow,threads_retriever
 from langchain_core.messages import HumanMessage
 import uuid
 
@@ -34,7 +34,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = threads_retriever()
 
 add_thread(st.session_state['thread_id'])
 
